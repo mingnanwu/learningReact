b3a0ef2429884fc48f422b210b778974d2be1779
mingnan wu <mingnanwu@gmail.com>
2015-04-29 23:33:17 -0700

    no idea what is going on!

--- public/scripts/state.js
+++ public/scripts/state.js
@@ -1,21 +1,16 @@
 var ClickApp = React.createClass({
-        getInitialState:function(){
-            return {
-                clickCount: 0,
-            }
-        },
-        handleClick: function(){
-            this.setState({
-                clickCount: this.state.clickCount + 1
-            })
-        },
         render: function(){
             return (
                 <div>
-                <h2>click it</h2>
-            <button onClick={this.handleClick}>button</button>
-        <p>you clicked: {this.state.clickCount}</p>=
-    </div>
+                    <ButtonGroup>
+                        <DropdownButton bsStyle="success" title="Dropdown">
+                            <MenuItem key="1">Dropdown link</MenuItem>
+                            <MenuItem key="2">Dropdown link</MenuItem>
+                        </DropdownButton>
+                        <Button bsStyle="info">Middle</Button>
+                        <Button bsStyle="info">Right</Button>
+                    </ButtonGroup>
+                </div>
 )
 }
 });
